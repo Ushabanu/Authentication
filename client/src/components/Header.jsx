71f6a722ef4265ff6568de6ea31f44cd04143044
Ushabanu <ushabanut@gmail.com>
2025-04-09 18:56:25 +0530

    Your commit message

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,7 @@ import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
 
 const Header = () => {
-
-  const {userData} = useContext(AppContext)
+  const { userData } = useContext(AppContext);
 
   return (
     <div className="flex flex-col items-center mt-20 px-4 text-center text-gray-800 ">
@@ -14,15 +13,19 @@ const Header = () => {
         className="w-36 h-36 rounded-full mb-6"
       />
       <h1 className="flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2">
-        Hey {userData ? userData.name : 'Developer'}! 
+        Hey {userData ? userData.name : "Developer"}!
         <img src={assets.hand_wave} alt="" className="w-8 aspect-square" />
       </h1>
-      <h2 className="text-3xl sm:text-5xl font-semibold mb-4">Welcome to our app</h2>
+      <h2 className="text-3xl sm:text-5xl font-semibold mb-4">
+        Welcome to our app
+      </h2>
       <p className="mb-8 max-w-md">
         Let's start with a quick product tour and we will have you up and
         running in no time!
       </p>
-      <button className="border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-200 transition-all">Get Started</button>
+      <button className="border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-200 transition-all">
+        Get Started
+      </button>
     </div>
   );
 };
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,51 +1,52 @@
 import { createContext, useEffect, useState } from "react";
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import axios from "axios";
+import { toast } from "react-toastify";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+  axios.defaults.withCredentials = true;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const [isLoggedin, setIsLoggedin] = useState(false)
-    const [userData, setUserData] = useState(null)
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  const [userData, setUserData] = useState(null);
 
-    const getAuthState = async ()  => {
-        try {
-            const {data} = await axios.get(backendUrl + '/api/auth/is-auth')
+  const getAuthState = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/auth/is-auth");
 
-            if(data.success){
-                setIsLoggedin(true)
-                getUserData()
-            }
-        } catch (error) {
-            toast.error(error.message)
-        }
+      if (data.success) {
+        setIsLoggedin(true);
+        getUserData();
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-    const getUserData = async () => {
-        try {
-            const {data} = await axios.get(backendUrl + '/api/user/data')
-            data.success ? setUserData(data.userData) : toast.error(data.message)
-        } catch (error) {
-            toast.error(error.message)
-        }
+  const getUserData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/data");
+      data.success ? setUserData(data.userData) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-    useEffect(()=>{
-        getAuthState()
-    },[])
+  useEffect(() => {
+    getAuthState();
+  }, []);
 
-    const value = {
-        backendUrl,
-        isLoggedin,setIsLoggedin,
-        userData,setUserData,
-        getUserData
-    }
+  const value = {
+    backendUrl,
+    isLoggedin,
+    setIsLoggedin,
+    userData,
+    setUserData,
+    getUserData,
+  };
 
-    return(
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
